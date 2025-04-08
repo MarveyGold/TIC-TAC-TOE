3baa94e90f380566326b79a19edcf5b3d5f99ea2
Marvey <osoidaghemarvel@gmail.com>
2025-04-08 15:10:50 +0100

    Finished

--- app/components/main.js
+++ app/components/main.js
@@ -5,6 +5,7 @@ export default function Main() {
     const [board, setBoard] = useState(Array(9).fill(null));
     const [isNext, setIsNext] = useState(true);
     const [winner, setWinner] = useState(null);
+    const [text, setText] = useState(" ");
 
     function handleClick(index) {
         if (board[index] || winner) return;
@@ -12,7 +13,7 @@ export default function Main() {
         const newBoard = [...board];
         newBoard[index]= isNext ? "X" : "O";
         setBoard(newBoard);
-        setIsNext(isNext);
+        setIsNext(!isNext);
         
         const gameWinner = calculateWinner(newBoard);
         if (gameWinner) {
@@ -29,7 +30,9 @@ export default function Main() {
         for (let i = 0; i < lines.length; i++) {
             const [a, b, c] = lines[i];
             if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-                return squares[a];
+                setText((balanceDisplay ) =>
+                    balanceDisplay===" "? "The winner is" + " " + squares[a] :"The winner is" + " " + squares[a]
+            );
             }
         }
         return null;
@@ -37,6 +40,8 @@ export default function Main() {
         function resetGame() {
             setBoard(Array(9).fill(null));
             setIsNext(true);
+            setWinner(null);
+            setText(null);
             
         }
 
@@ -44,9 +49,13 @@ export default function Main() {
     return(
         
         <>
-        <span> <button className={styles.tic} onClick={handleClick(0)} >{board[0]} </button> <button className={styles.tic} onClick={handleClick(1)}>{board[1]}</button> <button className={styles.tic} onClick={handleClick(2)}>{board[2]}</button></span>
-        <span> <button className={styles.tic} onClick={handleClick(3)}>{board[3]}</button> <button className={styles.tic} onClick={handleClick(4)}>{board[4]}</button> <button className={styles.tic} onClick={handleClick(5)}>{board[5]}</button></span>
-        <span> <button className={styles.tic} onClick={handleClick(6)}>{board[6]}</button> <button className={styles.tic} onClick={handleClick(7)}>{board[7]}</button> <button className={styles.tic} onClick={handleClick(8)}>{board[8]}</button></span>
+        <p>{text}</p>
+        <div>
+        <span> <button className={styles.tic} onClick={() => handleClick(0)} >{board[0]} </button> <button className={styles.tic} onClick={() => handleClick(1)}>{board[1]}</button> <button className={styles.tic} onClick={() => handleClick(2)}>{board[2]}</button></span>
+        <span> <button className={styles.tic} onClick={() => handleClick(3)}>{board[3]}</button> <button className={styles.tic} onClick={() => handleClick(4)}>{board[4]}</button> <button className={styles.tic} onClick={() => handleClick(5)}>{board[5]}</button></span>
+        <span> <button className={styles.tic} onClick={() => handleClick(6)}>{board[6]}</button> <button className={styles.tic} onClick={() => handleClick(7)}>{board[7]}</button> <button className={styles.tic} onClick={() => handleClick(8)}>{board[8]}</button></span>
+        </div>
+        <button onClick={resetGame} className={styles.reset} >Reset</button>
         </>
     )
 }
